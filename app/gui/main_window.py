c68293f486f2d7df18423808e15209dad5292526
Aricelio Fernandes <ariceliodesouza@gmail.com>
2025-06-09 10:26:53 -0300

    refactor: streamline environment editing by delegating to Environment class

--- app/gui/main_window.py
+++ app/gui/main_window.py
@@ -25,9 +25,10 @@ from PyQt5.QtGui import QKeyEvent, QIcon
 from core.importer import Importer
 from core.executor import Executor
 from core.environment import EnvironmentManager
-from gui.main.configuration import Configuration
 from data.collection import CollectionRepository
 from data.environment import EnvironmentRepository
+from gui.main.configuration import Configuration
+from gui.main.environment import Environment
 
 # Desabilita avisos de SSL inseguros
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -40,7 +41,6 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('Ferramenta de Requisições HTTP')
         self.setGeometry(100, 100, 1200, 800)
 
-        # Defini o ícone da janela
         Configuration.set_icon(self, os)
 
         self.collections = []
@@ -62,64 +62,11 @@ class MainWindow(QMainWindow):
     # Função para editar as variaveis de ambiente no menu
     def update_edit_environments_menu(self):
         self.edit_environments_menu.clear()
+
         for env_name in self.environments.environments.keys():
             edit_action = QAction(env_name, self)
-            edit_action.triggered.connect(lambda checked, name=env_name: self.edit_environment(name))
-            self.edit_environments_menu.addAction(edit_action)
-
-    # Função para exibir o diálogo de edição de um ambiente específico
-    def edit_environment(self, environment_name):
-        
-        # Cria uma janela de diálogo
-        dialog = QDialog(self)
-        dialog.setWindowTitle(f'Editar Ambiente - {environment_name}')
-        dialog.setModal(True)
-        dialog.resize(400, 300)
-
-        layout = QVBoxLayout(dialog)
-
-        # TextEdit para as variáveis do ambiente
-        env_text = QPlainTextEdit()
-        env_text.setAccessibleName('Variáveis do Ambiente')
-        env_text.setTabChangesFocus(True)
-        layout.addWidget(env_text)
-
-        # Carrega as variáveis atuais no TextEdit
-        variables = self.environments.environments.get(environment_name, {})
-        variables_text = '\n'.join(f'{k}={v}' for k, v in variables.items())
-        env_text.setPlainText(variables_text)
-
-        # Botão para salvar
-        save_button = QPushButton('Salvar')
-        save_button.setAccessibleName('Botão Salvar Ambiente')
-        save_button.clicked.connect(lambda: self.save_environment_changes(environment_name, env_text.toPlainText(), dialog))
-        layout.addWidget(save_button)
-
-        dialog.exec_()
-
-    # Função para salvar as alterações feitas em um ambiente
-    def save_environment_changes(self, environment_name, variables_text, dialog):
-        
-        # Analisa as variáveis a partir do texto
-        variables = {}
-        for line in variables_text.strip().split('\n'):
-            if '=' in line:
-                key, value = line.split('=', 1)
-                variables[key.strip()] = value.strip()
-        
-        # Atualiza o ambiente
-        self.environments.environments[environment_name] = variables
-
-        # Salva os ambientes em arquivo
-        EnvironmentRepository.save(self, os)
-        
-        # Atualiza o combo box e o menu de edição
-        self.update_environment_combo()
-        self.update_edit_environments_menu()
-        
-        # Fecha o diálogo
-        dialog.accept()
-        QMessageBox.information(self, 'Sucesso', f'Ambiente "{environment_name}" atualizado com sucesso!')    
+            edit_action.triggered.connect(lambda checked, name=env_name: Environment.edit_environment(self, name))
+            self.edit_environments_menu.addAction(edit_action)   
 
     # Função para lidar com a mudança do método HTTP selecionado
     def on_method_changed(self, button):
